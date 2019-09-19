9e2717a8f74c7834fcf326622f14dc4b06dcad09
Long <longish222@gmail.com>
2019-09-19 00:23:44 -0400

    navbar, footer, home page

--- perfect-pair/src/App.js
+++ perfect-pair/src/App.js
@@ -2,24 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavBar from "./components/navbar.component";
+import NavbarPage from "./components/navbar.component";
 // import ExerciseList from "./components/exercises-list.component";
 // import EditExercise from "./components/edit-exercise.component";
 // import CreateExercise from "./components/create-exercise.component";
 import HomePage from "./components/home-page.component";
 import CreateUser from "./components/create-user.component";
 
+import FooterPage from "./components/footer.component";
+
 function App() {
   return (
     <Router>
-      <NavBar />
-      <br />
-      <div className="container">
+      <NavbarPage />
         <Route path="/" component={HomePage} />
         {/* <Route path="/edit/:id" component={EditExercise} /> */}
         {/* <Route path="/create" component={CreateExercise} /> */}
         <Route path="/user" component={CreateUser} />
-      </div>
+      <FooterPage />
     </Router>
   );
 }
--- perfect-pair/src/components/navbar.component.js
+++ perfect-pair/src/components/navbar.component.js
@@ -1,33 +1,78 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBBtn, MDBFormInline, MDBInputGroup } from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
-          ExcerTracker
-        </Link>
-        <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                Exercises
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/create" className="nav-link">
-                Create Exercise Log
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/user" className="nav-link">
-                Create User
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+class NavbarPage extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  return (
+    <Router>
+      <MDBNavbar color="default-color" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text"><MDBNavLink to="#!">Perfect Pair</MDBNavLink></strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav left>
+            {/* <MDBInputGroup
+              containerClassName="mb-3"
+              append={
+                <MDBBtn
+                  color="mdb-color"
+                  outline
+                  className="m-0 px-3 py-2 z-depth-0"
+                >
+                  BUTTON
+                </MDBBtn>
+              }
+              inputs={
+                <select className="browser-default custom-select">
+                  <option value="0">Choose...</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              }
+            /> */}
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBNavLink className="waves-effect waves-light" to="#!">
+                <MDBIcon fab icon="twitter" />
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="waves-effect waves-light" to="#!">
+                <MDBIcon fab icon="google-plus-g" />
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBDropdown>
+                <MDBDropdownToggle nav caret>
+                  <MDBIcon icon="user" />
+                </MDBDropdownToggle>
+                <MDBDropdownMenu right basic className="dropdown-default">
+                  <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    </Router>
     );
   }
 }
+
+export default NavbarPage;
