81f1bbf5993ffce6ef913accfebcb444fae28db4
Long <longish222@gmail.com>
2019-09-18 22:42:50 -0400

    adding home page and navbar

--- perfect-pair/src/App.js
+++ perfect-pair/src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/navbar.component";
-import ExerciseList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
+// import ExerciseList from "./components/exercises-list.component";
+// import EditExercise from "./components/edit-exercise.component";
+// import CreateExercise from "./components/create-exercise.component";
+import HomePage from "./components/home-page.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
@@ -14,9 +15,9 @@ function App() {
       <NavBar />
       <br />
       <div className="container">
-        <Route path="/" component={ExerciseList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
+        <Route path="/" component={HomePage} />
+        {/* <Route path="/edit/:id" component={EditExercise} /> */}
+        {/* <Route path="/create" component={CreateExercise} /> */}
         <Route path="/user" component={CreateUser} />
       </div>
     </Router>
